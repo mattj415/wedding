372eb83184e1c0c5691210a8ac95cea474f01640
mdjohn13 <mattj415@gmail.com>
2015-04-29 09:36:05 -0700

    node server integration v1

--- app/rsvp/rsvp.js
+++ app/rsvp/rsvp.js
@@ -3,18 +3,51 @@
 angular.module('myApp.rsvp', ['ngRoute','ngAnimate'])
 
 .config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/rsvp', {
+  $routeProvider.when('/rsvp/:hashkey', {
     templateUrl: 'rsvp/rsvp.html',
     controller: 'rsvpCtrl'
   });
 }])
 
-.controller('rsvpCtrl', [function() {
-        this.active = true;
-        this.email_map = {
-            "matt" : {
-                "display":"Matthew Johnson and Kristle McCracken!!!"
+.controller('rsvpCtrl', ['$routeParams','$location','$http','$q', function($routeParams, $location, $http, $q) {
+
+
+        this.attending_options = [
+            { label: 'Will Attend', value: "will" },
+            { label: 'Will Not Attend', value: "will_not" }
+        ];
+        this.attending = this.attending_options[0];
+        this.numberOptions = [
+            { label: '1', value: 1 },
+            { label: '2', value: 2 }
+        ];
+        this.number = this.numberOptions[0];
+        this.hashkey = $routeParams.hashkey;
+        this.submit = function(){
+            if ( this.attending.value == 'will'){
+                alert("Thanks! We look forward to seeing you at the wedding!");
+            } else {
+                alert("Well fuck you too!");
             }
-        }
+
+            $location.url('/');
+        };
+        this.guest = undefined;
+        this.getGuest = function() {
+            var guest = null;
+            var deferred = $q.defer();
+            if (guest !== null) deferred.resolve(guest);
+            $http.get('http://localhost:3000/wedding/' + this.hashkey)
+                .success(function(data) {
+                    console.log("got data", data);
+                    if (data.length) {
+                        deferred.resolve(data[0]);
+                    } else {
+                        deferred.reject();
+                    }
+                }).error(deferred.reject);
+            return deferred.promise;
+        };
+        this.guest = this.getGuest();
 
 }]);
---
+++ server.js
@@ -0,0 +1,59 @@
+var express   =    require("express");
+var mysql     =    require('mysql');
+var app       =    express();
+
+var pool      =    mysql.createPool({
+    connectionLimit : 100, //important
+    host     : 'localhost',
+    user     : 'root',
+    password : '',
+    database : 'wedding',
+    debug    :  false
+});
+
+function handle_database(req,res) {
+    var hashkey = req.params.hashkey;
+    pool.getConnection(function(err,connection){
+        if (err) {
+            connection.release();
+            res.json({"code" : 100, "status" : "Error in connection database"});
+            return;
+        }
+
+        console.log('connected as id ' + connection.threadId);
+
+        connection.query("select * from guests WHERE hashkey = ?",[hashkey],function(err,rows){
+            connection.release();
+            if(!err) {
+                res.json(rows);
+                console.log('success');
+                console.log(rows);
+            }
+        });
+
+        connection.on('error', function(err) {
+            res.json({"code" : 100, "status" : "Error in connection database"});
+            return;
+        });
+    });
+}
+app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:63342');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Pass to next layer of middleware
+    next();
+});
+app.get("/wedding/:hashkey",function(req,res){
+
+    handle_database(req,res);
+});
+
+app.listen(3000);
